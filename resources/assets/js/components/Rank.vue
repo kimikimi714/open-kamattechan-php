4432b8f21381add6782d77085afa4bb309bc6231
kimikimi714 <kimikimi714@gmail.com>
2016-11-21 23:43:50 +0900

    :+1: fix design of Rank component

--- resources/assets/js/components/Rank.vue
+++ resources/assets/js/components/Rank.vue
@@ -1,7 +1,16 @@
 <template>
 <div>
-  rank component
-  <comment></comment>
+  <div class="row">
+    <div class="col-md-4">
+      XXth
+    </div>
+    <div class="col-md-8">
+      header
+    </div>
+  </div>
+  <div class="row">
+    <comment></comment>
+  </div>
 </div>
 </template>
 
