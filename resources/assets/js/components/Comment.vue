a7db52e62dbf29e0a47e95b5c84b88bae578e276
kimikimi714 <kimikimi714@gmail.com>
2016-11-21 23:43:03 +0900

    :+1: fix style of Comment component

--- resources/assets/js/components/Comment.vue
+++ resources/assets/js/components/Comment.vue
@@ -1,6 +1,17 @@
 <template>
-<div>
-  comment component
+<div class="col-md-offset-4 col-md-8 panel panel-default">
+  <div class="panel-body">
+    comment component
+    comment component
+    comment component
+    comment component
+    comment component
+  </div>
+  <div class="panel-footer">
+    <p class="text-right">
+      review lines
+    </p>
+  </div>
 </div>
 </template>
 
