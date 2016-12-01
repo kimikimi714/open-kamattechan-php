6e77f86e3a76aa8f1c31bbecf2fa5328a24459f1
kimikimi714 <kimikimi714@gmail.com>
2016-12-01 21:23:48 +0900

    :shower: delete unused method

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class RankingController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Show weekly rankings.
      *
      * @return \Illuminate\Http\Response
      */
@@ -29,28 +29,7 @@ class RankingController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
+     * Show weekly / monthly / danger rankings.
      *
      * @param  string  $type
      * @return \Illuminate\Http\Response
@@ -70,39 +49,5 @@ class RankingController extends Controller
           ],
         ]
       ]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
