ac674f2bb569c7c9e5118ee4e25c310e602ed0ca
kimikimi714 <kimikimi714@gmail.com>
2016-12-01 19:21:44 +0900

    :+1: bind data to Comment.vue

--- resources/assets/js/components/Comment.vue
+++ resources/assets/js/components/Comment.vue
@@ -1,17 +1,21 @@
 <template>
-<div class="col-md-offset-4 col-md-8">
-  <div class="panel panel-default">
-    <div class="panel-body">
-      comment component
-      comment component
-      comment component
-      comment component
-      comment component
-    </div>
-    <div class="panel-footer text-right">
-      review lines
-    </div>
+<div class="panel panel-default">
+  <div class="panel-body">
+    {{ comment }}
+  </div>
+  <div class="panel-footer text-right">
+    {{ fileName }}
   </div>
 </div>
 </template>
 
+<script>
+  export default {
+    data() {
+      return {
+        comment: "comment component",
+        fileName: "hoge.php"
+      }
+    }
+  }
+</script>
