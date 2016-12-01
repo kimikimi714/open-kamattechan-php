aed7441ec154e462e467c1f955a1b11af9e45b22
kimikimi714 <kimikimi714@gmail.com>
2016-12-01 16:11:31 +0900

    :+1: create RankHeader.vue

---
+++ resources/assets/js/components/RankHeader.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="media">
+  <div class="media-left">
+    <a href="#">
+      <img class="media-object" src="/images/animal_usagi_white.png" alt="user" width="128" height="128">
+    </a>
+  </div>
+  <div class="media-body">
+    <h4 class="media-heading">name</h4>
+  </div>
+</div>
+</template>
