b89f4a07412ddddb52a4002f0322b9d20faadbb5
kimikimi714 <kimikimi714@gmail.com>
2016-12-18 23:17:51 +0900

    :+1: add migration files

---
+++ database/migrations/2016_12_18_134444_create_reviews_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('hosting_type')->default('github');
+            $table->string('repository_url');
+            $table->unsignedMediumInteger('number');
+            $table->string('title');
+            $table->unsignedInteger('author_id');
+            $table->unsignedTinyInteger('status')->default(0);
+            $table->timestamps();
+
+            $table->primary('id');
+            $table->unique(['hosting_type', 'repository', 'number']);
+            // $table->foreign('author_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
--- database/migrations/2016_12_18_134451_create_comments_table.php
+++ database/migrations/2016_12_18_134451_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email');
-            $table->string('url');
-            // $table->string('password');
-            // $table->rememberToken();
+            $table->string('comment_type');
+            $table->unsignedInteger('review_id');
+            $table->unsignedInteger('commented_user_id');
+            $table->string('file_name');
+            $table->unsignedInteger('line');
+            $table->text('content');
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 }
