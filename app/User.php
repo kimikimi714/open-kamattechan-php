ffbb3426f1f530e3b7f353c0afc8a33793a7f014
kimikimi714 <kimikimi714@gmail.com>
2016-12-25 21:14:36 +0900

    :+1: set mass assignable attributes to User

--- app/User.php
+++ app/User.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = ['name', 'email', 'url'];
+
   protected $table = 'users';
 }
