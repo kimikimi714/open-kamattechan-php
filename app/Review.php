9642aaf1fba8c89b36f2cf6741930dc05e90bc0e
kimikimi714 <kimikimi714@gmail.com>
2016-12-25 21:13:46 +0900

    :+1: set mass assignable attributes to Review

--- app/Review.php
+++ app/Review.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = ['status', 'hosting_type', 'repository_url', 'number', 'title', 'author_id'];
+
   protected $table = 'reviews';
 }
