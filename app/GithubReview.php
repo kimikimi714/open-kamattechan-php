fbc3951fa0c3bf2eb9242fe8f2a077ffbaf79525
kimikimi714 <kimikimi714@gmail.com>
2017-01-08 20:49:08 +0900

    :bug: get github owner and repo name from url

--- app/GithubReview.php
+++ app/GithubReview.php
@@ -7,13 +7,15 @@ class GithubReview extends Review
   const HOSTING_TYPE = 'github';
   const BASE_REPOSITORY_URL = 'https://github.com';
 
-  public function getOwner() {
+  protected $appends = ['owner', 'repo'];
+
+  public function getOwnerAttribute() {
     $path = parse_url($this->repository_url, PHP_URL_PATH);
     $parts = explode('/', $path);
     return $parts[1];
   }
 
-  public function getRepo() {
+  public function getRepoAttribute() {
     $path = parse_url($this->repository_url, PHP_URL_PATH);
     $parts = explode('/', $path);
     return $parts[2];
